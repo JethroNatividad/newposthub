364f480093bc1b1bbbec91332f49f11769747139
JethroSama <kel73896@gmail.com>
2022-04-06 19:40:48 +0800

    signup route add end

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -47,7 +47,7 @@ export default async function handler(req, res) {
 
         } catch (error) {
             console.log(error)
-            return res.status(400)
+            return res.status(400).end(error.message)
         }
 
     }
