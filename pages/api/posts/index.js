533a81c62deeb15567f2dd42e9cd119c9495d9e4
JethroSama <kel73896@gmail.com>
2022-04-06 22:01:56 +0800

    implement retrieve all posts

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -6,14 +6,26 @@ import verifyToken from "../../../lib/verifyToken"
 export default async function handler(req, res) {
     await dbConnect()
 
-    const { method, body } = req
+    const { method } = req
     switch (method) {
+        case 'GET':
+            getPosts(res)
+            break
         case 'POST':
             verifyToken(req, res, () => createPost(req, res))
             break
         default:
             res.setHeader('Allow', ['GET', 'POST'])
             res.status(405).end(`Method ${method} Not Allowed`)
+    }
+    async function getPosts(res) {
+        try {
+            const posts = await Post.find()
+            res.status(200).json({ error: null, posts })
+        } catch (error) {
+            console.log(error)
+            return res.status(403).end(error.message)
+        }
     }
     async function createPost(req) {
         const { body: { text }, user: { id } } = req
