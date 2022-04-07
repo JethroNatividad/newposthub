fbbeafae8ba1843807a018ba34f41a25d8adf889
JethroSama <kel73896@gmail.com>
2022-04-07 19:52:24 +0800

    add retrieve post by id

--- pages/api/posts/[id].js
+++ pages/api/posts/[id].js
@@ -1,8 +1,11 @@
+import Post from "../../../lib/models/Post"
+
 export default function handler(req, res) {
-    const { id } = req.query
+    const { method, query: { id } } = req
+
     switch (method) {
         case 'GET':
-            getPost()
+            getPost(id)
             break
         case 'PUT':
             updatePost()
@@ -11,6 +14,17 @@ export default function handler(req, res) {
             res.setHeader('Allow', ['GET', 'POST'])
             res.status(405).end(`Method ${method} Not Allowed`)
     }
+    async function getPost() {
+        try {
+            const post = await Post.findById(id).populate('author', ['username', '_id'])
+            if (!post) {
+                return res.status(404).end('Post not found')
+            }
+            return res.status(200).json({ error: null, post })
+        } catch (error) {
+            console.log(error)
+            return res.status(400).end(error.message)
+        }
+    }
     async function updatePost() {}
-    async function getPost() {}
 }
