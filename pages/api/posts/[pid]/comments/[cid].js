4f339649e1a5a762763c74bea5b0cb2543366480
JethroSama <kel73896@gmail.com>
2022-04-07 23:02:38 +0800

    implement delete comment

--- pages/api/posts/[pid]/comments/[cid].js
+++ pages/api/posts/[pid]/comments/[cid].js
@@ -17,8 +17,11 @@ export default async function handler(req, res) {
         case 'PUT':
             verifyToken(req, res, () => updateComment(req, res, cid))
             break
+        case 'DELETE':
+            verifyToken(req, res, () => deleteComment(req, res, cid))
+            break
         default:
-            res.setHeader('Allow', ['GET', 'PUT'])
+            res.setHeader('Allow', ['GET', 'PUT', 'DELETE'])
             res.status(405).end(`Method ${method} Not Allowed`)
     }
     async function getComment(cid) {
@@ -33,6 +36,7 @@ export default async function handler(req, res) {
             return res.status(400).end(error.message)
         }
     }
+
     async function updateComment(req, res, cid) {
         const { body: { text }, user: { id } } = req
         if (!text) {
@@ -42,7 +46,7 @@ export default async function handler(req, res) {
         try {
             const comment = await Comment.findOne({ _id: cid })
             if (!comment) {
-                return res.status(404).end('Post not found')
+                return res.status(404).end('Comment not found')
             }
             if (comment.author.toString() !== id) {
                 return res.status(403).end('You are not authorized to edit this comment')
@@ -50,6 +54,26 @@ export default async function handler(req, res) {
             comment.text = text
             await comment.save()
             return res.status(200).json({ error: null, comment })
+        } catch (error) {
+            console.log(error)
+            return res.status(400).end(error.message)
+        }
+    }
+
+    async function deleteComment(req, res, cid) {
+        const { user: { id } } = req
+
+        try {
+            const comment = await Comment.findOne({ _id: cid })
+            if (!comment) {
+                return res.status(404).end('Comment not found, already deleted')
+            }
+            if (comment.author.toString() !== id) {
+                return res.status(403).end('You are not authorized to delete this comment')
+            }
+
+            await comment.remove()
+            return res.status(200).end('Comment deleted')
         } catch (error) {
             console.log(error)
             return res.status(400).end(error.message)
