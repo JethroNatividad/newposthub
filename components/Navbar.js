f81e279d2d9418dcb0822c7f3460e5d3acc0ccbe
JethroSama <kel73896@gmail.com>
2022-04-08 18:41:23 +0800

    style nav right area

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ import { UserIcon } from '@heroicons/react/solid'
 
 const Navbar = () => {
     return (
-        <div className="flex h-14 justify-between items-center bg-secondary-dark">
+        <div className="flex h-14 justify-between items-center bg-secondary-dark px-5">
             {/* Logo */ }
             <div className="flex justify-center">
 
@@ -13,21 +13,21 @@ const Navbar = () => {
 
             {/* Middle area */ }
             <div className="flex justify-center space-x-3">
-                <div className="text-offwhite-100 cursor-pointer hover:bg-tertiary-dark px-3 py-1">
+                <div className="text-offwhite-100 cursor-pointer hover:bg-tertiary-dark px-3 py-1 rounded-lg">
                     <HomeIcon className="w-10 h-10" />
                 </div>
-                <div className="text-offwhite-100 cursor-pointer hover:bg-tertiary-dark px-3 py-1">
+                <div className="text-offwhite-100 cursor-pointer hover:bg-tertiary-dark px-3 py-1 rounded-lg">
                     <CollectionIcon className="w-10 h-10" />
                 </div>
             </div>
 
             {/* Right area */ }
             <div className="flex space-x-2">
-                <div className="text-offwhite-50">
-                    <UserIcon className="w-10 h-10" />
+                <div className="text-offwhite-50 hover:brightness-150 bg-tertiary-dark p-2 rounded-3xl cursor-pointer flex justify-center items-center">
+                    <UserIcon className="w-7 h-7" />
                 </div>
-                <div>
-                    <LogoutIcon className="w-10 h-10" />
+                <div className="text-offwhite-50 hover:brightness-150 bg-tertiary-dark p-2 rounded-3xl cursor-pointer justify-center items-center">
+                    <LogoutIcon className="w-7 h-7" />
                 </div>
             </div>
         </div>
