a4e13188380bc7810753af6a15e30322a7394544
JethroSama <kel73896@gmail.com>
2022-04-08 18:49:16 +0800

    style logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,12 +8,17 @@ const Navbar = () => {
             {/* Logo */ }
             <div className="flex justify-center">
 
-                <Link href="/" >POSTHUB</Link>
+                <Link href="/">
+                    <>
+                        <p className="text-offwhite-50 font-mono text-3xl font-bold sm:hidden">PH</p>
+                        <p className="text-offwhite-50 font-mono text-3xl font-bold hidden sm:block">POSTHUB</p>
+                    </>
+                </Link>
             </div>
 
             {/* Middle area */ }
-            <div className="flex justify-center space-x-3">
-                <div className="text-offwhite-100 cursor-pointer hover:bg-tertiary-dark px-3 py-1 rounded-lg">
+            <div className="flex justify-center space-x-1 md:space-x-3">
+                <div className="text-offwhite-100 cursor-pointer hover:bg-tertiary-dark px-1 md:px-3 py-1 rounded-lg">
                     <HomeIcon className="w-10 h-10" />
                 </div>
                 <div className="text-offwhite-100 cursor-pointer hover:bg-tertiary-dark px-3 py-1 rounded-lg">
