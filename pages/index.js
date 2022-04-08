76e60c3ccfbc4bb8f9d3e3b91d8a3e24e1f4c8fc
JethroSama <kel73896@gmail.com>
2022-04-08 19:00:20 +0800

    change bg

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div>
+    <div className='bg-primary-dark min-h-screen'>
       <Head>
         <title>Posthub</title>
         <meta name="description" content="Posthub" />
