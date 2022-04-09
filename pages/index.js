63b5865e40c92d1323389c2452a9e8a137a295bc
JethroSama <kel73896@gmail.com>
2022-04-09 21:38:32 +0800

    add axios private, auto refresh access token

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,31 @@
 import Head from 'next/head'
+import { useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import useAuth from '../hooks/useAuth'
+import useAxiosPrivate from '../hooks/useAxiosPrivate'
+import { useRouter } from 'next/router'
 
 export default function Home() {
-  const { auth } = useAuth()
+  const { auth, setUser } = useAuth()
+  const axiosPrivate = useAxiosPrivate()
+  const router = useRouter()
+
+  useEffect(() => {
+    const fn = async () => {
+      const { data } = await axiosPrivate.get('/auth/user')
+      const user = data?.user
+
+      if (user) {
+        setUser(user)
+      } else {
+        router.push('/login')
+      }
+    }
+
+    fn()
+  }, [])
+
+
   return (
     <div className='bg-primary-dark min-h-screen mt-14'>
       <Head>
