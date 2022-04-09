c23ed59054c25c2b1fc0850e7b06bb85786668af
JethroSama <kel73896@gmail.com>
2022-04-10 00:11:03 +0800

    add nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import { AuthProvider } from '../lib/AuthProvider'
 import '../styles/globals.css'
-
+import NProgress from 'nprogress' //nprogress module
+import '../styles/nprogress.css' //styles of nprogress
+import Router from 'next/router'
+//Binding events. 
+NProgress.configure({ showSpinner: false })
+Router.events.on('routeChangeStart', () => NProgress.start()); Router.events.on('routeChangeComplete', () => NProgress.done()); Router.events.on('routeChangeError', () => NProgress.done())
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
