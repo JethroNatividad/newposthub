5e1c9e168679c08bfa9491e22342ea4253e57d2e
JethroSama <kel73896@gmail.com>
2022-04-12 23:43:21 +0800

    Setup react toast

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ import { UserIcon } from '@heroicons/react/solid'
 import apiAxios from "../lib/axios"
 import { useRouter } from 'next/router'
 import nprogress from "nprogress"
-
+import { toast } from "react-toastify"
 
 const Navbar = ({ user }) => {
     const router = useRouter()
@@ -18,6 +18,7 @@ const Navbar = ({ user }) => {
     return (
         <div className="flex shadow-md shadow-primary-dark h-14 w-full justify-between items-center bg-secondary-dark px-5 sticky top-0 right-0">
             {/* Logo */ }
+
             <div className="flex justify-center">
 
                 <Link href="/">
@@ -44,7 +45,10 @@ const Navbar = ({ user }) => {
 
             {/* Right area */ }
             <div className="flex space-x-2">
-                <div className="text-offwhite-50 hidden sm:flex hover:brightness-150 bg-tertiary-dark pl-1 pr-3 rounded-3xl cursor-pointer justify-center items-center">
+                <div onClick={ () => {
+                    toast("IT WORKS")
+
+                } } className="text-offwhite-50 hidden sm:flex hover:brightness-150 bg-tertiary-dark pl-1 pr-3 rounded-3xl cursor-pointer justify-center items-center">
                     <div className="text-offwhite-50 hover:brightness-150 bg-tertiary-dark p-1 rounded-3xl cursor-pointer flex justify-center items-center">
                         <UserIcon className="w-5 h-5" />
                     </div>
@@ -52,7 +56,9 @@ const Navbar = ({ user }) => {
 
                 </div>
 
-                <div className="text-offwhite-50 sm:hidden hover:brightness-150 bg-tertiary-dark p-2 rounded-3xl cursor-pointer flex justify-center items-center">
+                <div onClick={ () => {
+                    toast("IT WORKS")
+                } } className="text-offwhite-50 sm:hidden hover:brightness-150 bg-tertiary-dark p-2 rounded-3xl cursor-pointer flex justify-center items-center">
                     <UserIcon className="w-7 h-7" />
                 </div>
 
--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,27 @@ import '../styles/globals.css'
 import NProgress from 'nprogress' //nprogress module
 import '../styles/nprogress.css' //styles of nprogress
 import Router from 'next/router'
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify'
+
 //Binding events. 
 NProgress.configure({ showSpinner: false })
 Router.events.on('routeChangeStart', () => NProgress.start()); Router.events.on('routeChangeComplete', () => NProgress.done()); Router.events.on('routeChangeError', () => NProgress.done())
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <ToastContainer
+        position="top-right"
+        autoClose={ 5000 }
+        hideProgressBar={ false }
+        newestOnTop={ false }
+        closeOnClick
+        rtl={ false }
+        pauseOnFocusLoss={ false }
+        draggable
+        pauseOnHover={ false }
+        theme="dark"
+      />
       <Component { ...pageProps } />
     </AuthProvider>
   )
