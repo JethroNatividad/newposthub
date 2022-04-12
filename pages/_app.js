3d15e6f97f1a62aac8804ddbb70ee0c62c8c43bc
JethroSama <kel73896@gmail.com>
2022-04-13 00:05:47 +0800

    style toast

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,9 @@
+import Router from 'next/router'
+import NProgress from 'nprogress' //nprogress module
+import ToastContainer from '../components/ToastContainer'
 import { AuthProvider } from '../lib/AuthProvider'
 import '../styles/globals.css'
-import NProgress from 'nprogress' //nprogress module
 import '../styles/nprogress.css' //styles of nprogress
-import Router from 'next/router'
-import 'react-toastify/dist/ReactToastify.css'
-import { ToastContainer } from 'react-toastify'
 
 //Binding events. 
 NProgress.configure({ showSpinner: false })
@@ -12,18 +11,7 @@ Router.events.on('routeChangeStart', () => NProgress.start()); Router.events.on(
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <ToastContainer
-        position="top-right"
-        autoClose={ 5000 }
-        hideProgressBar={ false }
-        newestOnTop={ false }
-        closeOnClick
-        rtl={ false }
-        pauseOnFocusLoss={ false }
-        draggable
-        pauseOnHover={ false }
-        theme="dark"
-      />
+      <ToastContainer />
       <Component { ...pageProps } />
     </AuthProvider>
   )
