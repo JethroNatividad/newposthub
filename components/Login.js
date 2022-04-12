28c9d9155ef2ea9dc7265582862d6bafa237e500
JethroSama <kel73896@gmail.com>
2022-04-13 00:40:11 +0800

    disable button upon login

--- components/Login.js
+++ components/Login.js
@@ -25,31 +25,27 @@ const Login = () => {
 
                     <Formik initialValues={ { usernameOrEmail: '', password: '' } }
                         onSubmit={ async ({ usernameOrEmail, password }, { setSubmitting }) => {
-                            try {
-                                Nprogress.start()
-                                setSubmitting(true)
-                                // const res = await axios.post('/auth/login', { usernameOrEmail, password })
-                                const [err, data] = await poster('/auth/login', { usernameOrEmail, password })
-                                console.log(data)
-                                if (err) {
-                                    toast.error(err.message)
-                                    Nprogress.done()
-                                    return setSubmitting(false)
-                                }
+                            Nprogress.start()
+                            setSubmitting(true)
+                            // const res = await axios.post('/auth/login', { usernameOrEmail, password })
+                            const [err, data] = await poster('/auth/login', { usernameOrEmail, password })
+                            console.log(data)
+                            if (err) {
+                                toast.error(err.message)
                                 Nprogress.done()
-                                router.push('/')
-                                toast.success("Welcome back, " + data.user.username, { delay: 1000 })
-                                setSubmitting(false)
-                            } catch (error) {
-                                console.log(error)
+                                return setSubmitting(false)
                             }
+                            Nprogress.done()
+                            router.push('/')
+                            toast.success("Welcome back, " + data.user.username, { delay: 1000 })
+                            setSubmitting(false)
                         } }>
                         { ({ values, handleChange, isSubmitting, handleSubmit }) => (
                             <form className='flex flex-col space-y-3' onSubmit={ handleSubmit }>
 
                                 <input className=' px-4 py-3 rounded-lg outline-none text-md md:text-xl text-offwhite-50 bg-tertiary-dark' placeholder='Username or Email' type="text" name="usernameOrEmail" value={ values.usernameOrEmail } onChange={ handleChange } />
                                 <input className=' px-4 py-3 rounded-lg outline-none text-md md:text-xl text-offwhite-50 bg-tertiary-dark' placeholder='Password' type="password" name="password" value={ values.password } onChange={ handleChange } />
-                                <button className='px-4 hover:brightness-110 mb-5 py-3 rounded-lg outline-none text-md md:text-xl font-bold text-offwhite-50 bg-primary-dark' type="submit">Log In</button>
+                                <button disabled={ isSubmitting } className='px-4 hover:brightness-110 mb-5 py-3 rounded-lg outline-none text-md md:text-xl font-bold text-offwhite-50 bg-primary-dark' type="submit">Log In</button>
 
                             </form>
                         ) }
