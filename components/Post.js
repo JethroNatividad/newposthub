b246e3cdc15bb1ce413b311768e3b11bbdd4b473
JethroSama <kel73896@gmail.com>
2022-04-15 16:27:40 +0800

    Add link to singlepost

--- components/Post.js
+++ components/Post.js
@@ -18,6 +18,7 @@ const Post = ({ data, user, deletePost }) => {
         deletePost(_id)
     }
 
+
     console.log(data)
     return (
         <div className='w-full bg-secondary-dark rounded-lg text-offwhite-50'>
@@ -50,16 +51,18 @@ const Post = ({ data, user, deletePost }) => {
             {/* Comments button */ }
             <div className='p-2 md:p-4 space-y-3 flex flex-col'>
                 <div className='flex justify-end items-center'>
-                    { commentsCount > 0 && (<p className='text-sm text-offwhite-100'>{ commentsCount } { commentsCount > 1 ? "Comments" : "Comment" }</p>) }
+                    { commentsCount > 0 && (<Link href={ `/post/${_id}` } ><p className='text-sm text-offwhite-100 hover:underline cursor-pointer'>{ commentsCount } { commentsCount > 1 ? "Comments" : "Comment" }</p></Link>) }
                 </div>
 
                 <div className='w-full  h-[1px] bg-offwhite-50' />
 
                 <div className='flex justify-center items-center'>
-                    <div className="text-offwhite-100 cursor-pointer flex justify-center space-x-1 hover:bg-tertiary-dark px-3 py-1 rounded-lg w-40">
-                        <ChatAltIcon className="w-7 h-7" />
-                        <p className='font-semibold'>Comment</p>
-                    </div>
+                    <Link href={ `/post/${_id}` } >
+                        <div className="text-offwhite-100 cursor-pointer flex justify-center space-x-1 hover:bg-tertiary-dark px-3 py-1 rounded-lg w-40">
+                            <ChatAltIcon className="w-7 h-7" />
+                            <p className='font-semibold'>Comment</p>
+                        </div>
+                    </Link>
                 </div>
 
             </div>
