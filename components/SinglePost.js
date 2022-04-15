b1d7f51181ba6d04b4d3b5ba46a4b9cc3a3265a1
JethroSama <kel73896@gmail.com>
2022-04-15 16:50:25 +0800

    add singlepost

---
+++ components/SinglePost.js
@@ -0,0 +1,71 @@
+import { DotsHorizontalIcon, UserIcon } from '@heroicons/react/solid'
+import { ChatAltIcon, PencilAltIcon, TrashIcon } from '@heroicons/react/outline'
+import moment from 'moment'
+import Link from "next/link"
+import ConfirmationButton from './ConfirmationButton'
+import DotsMenu from './DotsMenu'
+
+
+const SinglePost = ({ data, loading, user }) => {
+    const text = data?.text
+    const author = data?.author
+    const comments = data?.comments
+    const updatedAt = data?.updatedAt
+    const createdAt = data?.createdAt
+    const _id = data?._id
+    const commentsCount = comments?.length
+    const isAuthor = author?._id === user.id
+    const timePassed = moment(createdAt).fromNow()
+    const isEdited = updatedAt !== createdAt
+
+    if (loading) return <p>Loading</p>
+    return (
+        <div className='w-full bg-secondary-dark rounded-lg text-offwhite-50'>
+            {/* Author */ }
+            <div className='flex p-2 md:p-4 justify-between'>
+                <div className='flex space-x-2'>
+                    <div className="text-offwhite-50 hover:brightness-150 bg-tertiary-dark p-2 rounded-3xl cursor-pointer flex justify-center items-center">
+                        <UserIcon className="w-7 h-7" />
+                    </div>
+                    <div>
+                        <p className='font-semibold'>{ author.username }</p>
+                        <p className='text-sm text-offwhite-100'>{ timePassed } { isEdited && <span className='text-xs text-gray-500'> • edited</span> }</p>
+                    </div>
+                </div>
+
+                {
+                    isAuthor && <DotsMenu>
+                        <button className='px-4 flex justify-center hover:brightness-110 py-1 w-full rounded-lg outline-none text-md md:text-xl text-offwhite-100 bg-primary-dark max-w-xs hover:text-orange-400' type="submit"><Link href={ `/post/${_id}/edit` } ><PencilAltIcon className="w-6 h-6 relative" /></Link></button>
+                        <ConfirmationButton handleDelete={ handleDelete } />
+                    </DotsMenu>
+                }
+            </div>
+
+            {/* Content */ }
+            <div className='p-2 md:p-4'>
+                <p>{ text }</p>
+            </div>
+
+            {/* Comments button */ }
+            <div className='p-2 md:p-4 space-y-3 flex flex-col'>
+                <div className='flex justify-end items-center'>
+                    { commentsCount > 0 && (<Link href={ `/post/${_id}` } ><p className='text-sm text-offwhite-100 hover:underline cursor-pointer'>{ commentsCount } { commentsCount > 1 ? "Comments" : "Comment" }</p></Link>) }
+                </div>
+
+                <div className='w-full  h-[1px] bg-offwhite-50' />
+
+                <div className='flex justify-center items-center'>
+                    <Link href={ `/post/${_id}` } >
+                        <div className="text-offwhite-100 cursor-pointer flex justify-center space-x-1 hover:bg-tertiary-dark px-3 py-1 rounded-lg w-40">
+                            <ChatAltIcon className="w-7 h-7" />
+                            <p className='font-semibold'>Comment</p>
+                        </div>
+                    </Link>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default SinglePost
--- pages/post/[pid]/index.js
+++ pages/post/[pid]/index.js
@@ -1,6 +1,9 @@
-import React from 'react'
 import Navbar from '../../../components/Navbar'
+import SinglePost from '../../../components/SinglePost'
 import fetcherSSR from '../../../lib/fetcherSSR'
+import { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
+import fetcher from '../../../lib/fetcher'
 
 export async function getServerSideProps({ req, res, query }) {
     const [error, user] = await fetcherSSR(req, res, '/auth/user')
@@ -15,15 +18,24 @@ export async function getServerSideProps({ req, res, query }) {
 const index = ({ user, query }) => {
     // get the query params
     const { pid } = query
+    const [post, setPost] = useState(null)
+
+    useEffect(() => {
+        const fn = async () => {
+            const [err, data] = await fetcher(`/posts/${pid}`)
+            if (err) {
+                return toast.error(err.message)
+            }
+            console.log(data, "POST DATA SINGLE")
+            setPost(data.post)
+        }
+        fn()
+    }, [pid])
 
     return (
         <div className='bg-primary-dark min-h-screen'>
             <Navbar user={ user } />
-            <div>
-                <p>
-                    You are viewing post { pid }
-                </p>
-            </div>
+            <SinglePost user={ user } data={ post } loading={ post === null } />
         </div>
     )
 }
