4d0f49b92ed73127fa322618a9fc75487fec5258
JethroSama <kel73896@gmail.com>
2022-04-15 18:34:44 +0800

    add comment section

--- components/CommentSection.js
+++ components/CommentSection.js
@@ -1,8 +1,36 @@
+import { ChevronRightIcon, UserIcon } from '@heroicons/react/solid'
+import { Formik } from 'formik'
 import React from 'react'
 
 const CommentSection = () => {
+    const handleSubmit = async () => {
+
+    }
     return (
-        <div>CommentSection</div>
+        <div className='w-full'>
+            <div className='mb-3'>
+                <div className='flex'>
+                    <div className="text-offwhite-50 w-10 h-10 mr-1 hover:brightness-150 bg-tertiary-dark rounded-full cursor-pointer flex justify-center items-center">
+                        <UserIcon className="w-6 h-6" />
+                    </div>
+                    <div className='bg-tertiary-dark py-1 px-2 rounded-xl w-full max-w-xl flex-1'>
+                        <p className='font-bold text-sm'>Username</p>
+                        <p className='text-md'>Comment Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, cum. Lorem ipsum dolor sit amet.</p>
+                    </div>
+                </div>
+            </div>
+            <Formik initialValues={ { text: '', } }
+                onSubmit={ handleSubmit }>
+                { ({ values, handleChange, isSubmitting, handleSubmit }) => (
+                    <form className='flex items-center' onSubmit={ handleSubmit }>
+
+                        <input className=' px-4 py-3 w-full rounded-lg outline-none text-md md:text-md text-offwhite-50 bg-tertiary-dark' placeholder='Comment' type="text" name="text" value={ values.text } onChange={ handleChange } />
+                        <button disabled={ isSubmitting } className='hover:brightness-110 flex items-center py-3 px-3 rounded-lg outline-none font-bold text-offwhite-50 bg-primary-dark' type="submit"><ChevronRightIcon height="24" width="24" /></button>
+
+                    </form>
+                ) }
+            </Formik>
+        </div>
     )
 }
 
--- components/SinglePost.js
+++ components/SinglePost.js
@@ -21,44 +21,47 @@ const SinglePost = ({ data, loading, user }) => {
 
     if (loading) return <p>Loading</p>
     return (
-        <div className='w-full max-w-3xl mx-3 md:mx-auto bg-secondary-dark rounded-lg text-offwhite-50'>
-            {/* Author */ }
-            <div className='flex p-2 md:p-4 justify-between'>
-                <div className='flex space-x-2'>
-                    <div className="text-offwhite-50 hover:brightness-150 bg-tertiary-dark p-2 rounded-3xl cursor-pointer flex justify-center items-center">
-                        <UserIcon className="w-7 h-7" />
+        <div className='p-3'>
+
+            <div className='w-full max-w-3xl md:mx-auto bg-secondary-dark rounded-lg text-offwhite-50'>
+                {/* Author */ }
+                <div className='flex p-2 md:p-4 justify-between'>
+                    <div className='flex space-x-2'>
+                        <div className="text-offwhite-50 hover:brightness-150 bg-tertiary-dark p-2 rounded-3xl cursor-pointer flex justify-center items-center">
+                            <UserIcon className="w-7 h-7" />
+                        </div>
+                        <div>
+                            <p className='font-semibold'>{ author.username }</p>
+                            <p className='text-sm text-offwhite-100'>{ timePassed } { isEdited && <span className='text-xs text-gray-500'> • edited</span> }</p>
+                        </div>
                     </div>
-                    <div>
-                        <p className='font-semibold'>{ author.username }</p>
-                        <p className='text-sm text-offwhite-100'>{ timePassed } { isEdited && <span className='text-xs text-gray-500'> • edited</span> }</p>
+
+                    {
+                        isAuthor && <DotsMenu>
+                            <button className='px-4 flex justify-center hover:brightness-110 py-1 w-full rounded-lg outline-none text-md md:text-xl text-offwhite-100 bg-primary-dark max-w-xs hover:text-orange-400' type="submit"><Link href={ `/post/${_id}/edit` } ><PencilAltIcon className="w-6 h-6 relative" /></Link></button>
+                            <ConfirmationButton handleDelete={ handleDelete } />
+                        </DotsMenu>
+                    }
+                </div>
+
+                {/* Content */ }
+                <div className='p-2 md:p-4'>
+                    <p>{ text }</p>
+                </div>
+
+                {/* Comments button */ }
+                <div className='p-2 md:p-4 space-y-3 flex flex-col'>
+                    <div className='flex justify-end items-center'>
+                        { commentsCount > 0 && (<Link href={ `/post/${_id}` } ><p className='text-sm text-offwhite-100 hover:underline cursor-pointer'>{ commentsCount } { commentsCount > 1 ? "Comments" : "Comment" }</p></Link>) }
                     </div>
+
+                    <div className='w-full  h-[1px] bg-offwhite-50' />
+
+                    <div className='flex justify-center items-center'>
+                        <CommentSection />
+                    </div>
+
                 </div>
-
-                {
-                    isAuthor && <DotsMenu>
-                        <button className='px-4 flex justify-center hover:brightness-110 py-1 w-full rounded-lg outline-none text-md md:text-xl text-offwhite-100 bg-primary-dark max-w-xs hover:text-orange-400' type="submit"><Link href={ `/post/${_id}/edit` } ><PencilAltIcon className="w-6 h-6 relative" /></Link></button>
-                        <ConfirmationButton handleDelete={ handleDelete } />
-                    </DotsMenu>
-                }
-            </div>
-
-            {/* Content */ }
-            <div className='p-2 md:p-4'>
-                <p>{ text }</p>
-            </div>
-
-            {/* Comments button */ }
-            <div className='p-2 md:p-4 space-y-3 flex flex-col'>
-                <div className='flex justify-end items-center'>
-                    { commentsCount > 0 && (<Link href={ `/post/${_id}` } ><p className='text-sm text-offwhite-100 hover:underline cursor-pointer'>{ commentsCount } { commentsCount > 1 ? "Comments" : "Comment" }</p></Link>) }
-                </div>
-
-                <div className='w-full  h-[1px] bg-offwhite-50' />
-
-                <div className='flex justify-center items-center'>
-                    <CommentSection />
-                </div>
-
             </div>
         </div>
     )
