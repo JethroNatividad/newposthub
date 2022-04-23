db0994f24897cb6e95f91b8e9dd270df65ec64a1
JethroSama <kel73896@gmail.com>
2022-04-23 22:45:50 +0800

    added react lightbox

--- components/Images.js
+++ components/Images.js
@@ -1,7 +1,12 @@
 import Image from 'next/image'
-import React from 'react'
+import { useState } from 'react'
+import Lightbox from 'react-image-lightbox'
+import 'react-image-lightbox/style.css'
 
 const Images = ({ images, countFrom = 5 }) => {
+    const [isOpen, setIsOpen] = useState(true)
+    const [photoIndex, setphotoIndex] = useState(0)
+
     // const imagesToShow = images
     // if (countFrom && images.length > countFrom) {
     //     imagesToShow.length = countFrom
@@ -13,22 +18,40 @@ const Images = ({ images, countFrom = 5 }) => {
     // For five pictures, two in the first row, three in the second row
     // For more than five, it should display an overlay presenting the counts of additional images
 
-
+    const handleOpen = (index) => {
+        setphotoIndex(index)
+        setIsOpen(true)
+    }
     const renderOneImage = () => {
         return (
             <div className='grid grid-cols-1 h-[576px]'>
-                <div className='relative h-full '>
+                <div onClick={ () => handleOpen(0) } className='relative h-full cursor-pointer'>
                     <Image className='object-contain' src={ images[0] } layout="fill" alt="img" />
                 </div>
             </div>)
     }
 
 
+
     return (
         <div className='w-full shadow-sm shadow-tertiary-dark'>
             { renderOneImage() }
+            { isOpen && (
+                <Lightbox
+                    onImageLoad={ () => console.log('Image loaded') }
+                    mainSrc={ images[photoIndex] }
+                    nextSrc={ images[(photoIndex + 1) % images.length] }
+                    prevSrc={ images[(photoIndex + images.length - 1) % images.length] }
+                    onCloseRequest={ () => setIsOpen(false) }
+                    onMovePrevRequest={ () =>
+                        setphotoIndex((photoIndex + images.length - 1) % images.length)
+                    }
+                    onMoveNextRequest={ () =>
+                        setphotoIndex((photoIndex + 1) % images.length)
 
-            {/* { images.map(image => <p>{ image }</p>) } */ }
+                    }
+                />
+            ) }
         </div>
     )
 }
