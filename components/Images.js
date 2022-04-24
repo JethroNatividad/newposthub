123ab260fffb3e9516efa5631e5562806a1eb39f
JethroSama <kel73896@gmail.com>
2022-04-24 18:38:54 +0800

    add for three images

--- components/Images.js
+++ components/Images.js
@@ -83,11 +83,66 @@ const Images = ({ images, countFrom = 5 }) => {
         )
     }
 
+    const renderThreeImage = () => {
+        const { url, height, width } = images[0]
+        return (
+            <Gallery>
+                <div className='grid grid-cols-2 h-[576px]'>
+                    <Item
+                        original={ url }
+                        width={ width }
+                        height={ height }
+                        objectFit='cover'
+
+                    >
+                        { ({ ref, open, }) => (
+                            // <img ref={ref} onClick={open} src="https://placekitten.com/80/60?image=1" />
+                            <div ref={ ref } onClick={ open } className='relative h-full cursor-pointer'>
+                                <Image className='object-cover' src={ url } layout="fill" alt="img" />
+                            </div>
+                        ) }
+                    </Item>
+                    <div className='h-full'>
+
+                        <Item
+                            original={ url }
+                            width={ width }
+                            height={ height }
+                            objectFit='cover'
+
+                        >
+                            { ({ ref, open, }) => (
+                                // <img ref={ref} onClick={open} src="https://placekitten.com/80/60?image=1" />
+                                <div ref={ ref } onClick={ open } className='relative h-1/2 cursor-pointer'>
+                                    <Image className='object-cover' src={ url } layout="fill" alt="img" />
+                                </div>
+                            ) }
+                        </Item>
+                        <Item
+                            original={ url }
+                            width={ width }
+                            height={ height }
+                            objectFit='cover'
+
+                        >
+                            { ({ ref, open, }) => (
+                                // <img ref={ref} onClick={open} src="https://placekitten.com/80/60?image=1" />
+                                <div ref={ ref } onClick={ open } className='relative h-1/2 cursor-pointer'>
+                                    <Image className='object-cover' src={ url } layout="fill" alt="img" />
+                                </div>
+                            ) }
+                        </Item>
+                    </div>
+                </div>
+            </Gallery>
+        )
+    }
+
 
 
     return (
         <div className='w-full shadow-sm shadow-tertiary-dark'>
-            { renderTwoImage() }
+            { renderThreeImage() }
 
         </div>
     )
