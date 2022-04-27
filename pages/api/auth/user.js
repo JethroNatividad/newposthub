94a4c504c396851bf2f9bb25703752367c4e7ce7
JethroSama <kel73896@gmail.com>
2022-04-27 19:32:14 +0800

    api pull other data from user

--- pages/api/auth/user.js
+++ pages/api/auth/user.js
@@ -1,3 +1,4 @@
+import User from "../../../lib/models/User"
 import verifyToken from "../../../lib/verifyToken"
 
 export default async function handler(req, res) {
@@ -12,6 +13,18 @@ export default async function handler(req, res) {
     }
     async function getUser(req, res) {
         const { user } = req
-        return res.status(200).json({ error: null, user })
+        try {
+            const currentUser = await User.findOne({ _id: user.id })
+            // remove the password from the response
+            currentUser.hashedPassword = undefined
+            if (!currentUser) {
+                return res.status(404).end('User not found')
+            }
+            return res.status(200).json({ error: null, user: currentUser })
+        } catch (error) {
+            console.log(error)
+            return res.status(400).end(error.message)
+        }
+
     }
 }
