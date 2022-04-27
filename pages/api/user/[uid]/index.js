58bc2c9e9c6b4bad7c17041c1d557443bb302a46
JethroSama <kel73896@gmail.com>
2022-04-27 17:11:29 +0800

    imports

--- pages/api/user/[uid]/index.js
+++ pages/api/user/[uid]/index.js
@@ -1,5 +1,6 @@
 import dbConnect from "../../../../lib/dbConnect"
 import User from "../../../../lib/models/User"
+import verifyToken from "../../../../lib/verifyToken"
 
 
 export default async function handler(req, res) {
