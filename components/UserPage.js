85c450fb955031cb715ee95cd5be6d93d3622eaa
JethroSama <kel73896@gmail.com>
2022-04-29 22:13:45 +0800

    show username

--- components/UserPage.js
+++ components/UserPage.js
@@ -23,8 +23,8 @@ const UserPage = ({ currentUser, uid }) => {
 
     return (
         <div className='p-3'>
-            <div className='w-full h-96 max-w-7xl md:mx-auto bg-secondary-dark rounded-lg text-offwhite-50 flex flex-col items-center py-2'>
-                <div className='h-20 w-20 sm:h-32 sm:w-32 lg:h-40 lg:w-40 rounded-full overflow-hidden cursor-pointer flex'>
+            <div className='w-full max-w-5xl md:mx-auto bg-secondary-dark rounded-lg text-offwhite-50 flex flex-col items-center py-2'>
+                <div className='h-24 w-24 sm:h-32 sm:w-32 lg:h-40 lg:w-40 rounded-full overflow-hidden cursor-pointer flex'>
                     { loading ? <Skeleton className='leading-loose' height='160px' width='160px' /> : <div className="text-offwhite-50 hover:brightness-150 w-full h-full flex items-center justify-center bg-tertiary-dark">
                         <div className="relative bg-primary-dark h-full w-full rounded-full overflow-hidden">
                             <Image src='https://res.cloudinary.com/jethrosama/image/upload/v1651059504/newposthub/profile_pictures/images_ufarco.png' layout="fill" />
@@ -32,7 +32,7 @@ const UserPage = ({ currentUser, uid }) => {
                     </div> }
                 </div>
 
-                <h1>
+                <h1 className='font-semibold text-lg sm:text-2xl lg:text-3xl'>
                     { loading ? <Skeleton width={ 60 } /> : user.username }
                 </h1>
             </div>
