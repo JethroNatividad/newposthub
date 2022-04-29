1405cca299f59c4f5aa4fb781baef86a531a8137
JethroSama <kel73896@gmail.com>
2022-04-29 22:05:37 +0800

    style profile pic

--- components/UserPage.js
+++ components/UserPage.js
@@ -13,7 +13,7 @@ const UserPage = ({ currentUser, uid }) => {
         if (err) {
             return console.error(err)
         }
-        setUser(user)
+        setUser(user.user)
         setLoading(false)
     }
 
@@ -23,14 +23,18 @@ const UserPage = ({ currentUser, uid }) => {
 
     return (
         <div className='p-3'>
-            <div className='w-full h-96 max-w-7xl md:mx-auto bg-secondary-dark rounded-lg text-offwhite-50 flex justify-center'>
-                <div className='h-16 w-16 rounded-full overflow-hidden cursor-pointer flex'>
-                    { loading ? <Skeleton className='leading-loose' height='64px' width='64px' /> : <div className="text-offwhite-50 hover:brightness-150 w-full h-full flex items-center justify-center bg-tertiary-dark">
+            <div className='w-full h-96 max-w-7xl md:mx-auto bg-secondary-dark rounded-lg text-offwhite-50 flex flex-col items-center py-2'>
+                <div className='h-20 w-20 sm:h-32 sm:w-32 lg:h-40 lg:w-40 rounded-full overflow-hidden cursor-pointer flex'>
+                    { loading ? <Skeleton className='leading-loose' height='160px' width='160px' /> : <div className="text-offwhite-50 hover:brightness-150 w-full h-full flex items-center justify-center bg-tertiary-dark">
                         <div className="relative bg-primary-dark h-full w-full rounded-full overflow-hidden">
                             <Image src='https://res.cloudinary.com/jethrosama/image/upload/v1651059504/newposthub/profile_pictures/images_ufarco.png' layout="fill" />
                         </div>
                     </div> }
                 </div>
+
+                <h1>
+                    { loading ? <Skeleton width={ 60 } /> : user.username }
+                </h1>
             </div>
 
         </div>
