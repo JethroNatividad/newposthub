b4558c207a73d1ae2114c1537f01ce31406b9001
JethroSama <kel73896@gmail.com>
2022-04-29 22:53:19 +0800

    Userpage: add message if empty

--- components/UserPage.js
+++ components/UserPage.js
@@ -1,12 +1,14 @@
 import { useState, useEffect } from 'react'
 import Skeleton from 'react-loading-skeleton'
-import fetcher from '../lib/fetcher'
+import fetcher, { deleter } from '../lib/fetcher'
 import Image from 'next/image'
 import PostList from './PostList'
+import { toast } from 'react-toastify'
 
 const UserPage = ({ currentUser, uid }) => {
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
+    const isOwner = currentUser?._id === user?._id
 
     const fetchUser = async () => {
         setLoading(true)
@@ -37,8 +39,8 @@ const UserPage = ({ currentUser, uid }) => {
     return (
         <div className='p-3'>
             <div className='w-full max-w-5xl md:mx-auto bg-secondary-dark rounded-lg text-offwhite-50 flex flex-col items-center py-2 mb-6'>
-                <div className='h-24 w-24 sm:h-32 sm:w-32 lg:h-40 lg:w-40 rounded-full overflow-hidden cursor-pointer flex'>
-                    { loading ? <Skeleton className='leading-loose' height='160px' width='160px' /> : <div className="text-offwhite-50 hover:brightness-150 w-full h-full flex items-center justify-center bg-tertiary-dark">
+                <div className='h-24 w-24 sm:h-32 sm:w-32 lg:h-40 lg:w-40 rounded-full overflow-hidden flex'>
+                    { loading ? <Skeleton className='leading-loose' height='160px' width='160px' /> : <div className="text-offwhite-50 w-full h-full flex items-center justify-center bg-tertiary-dark">
                         <div className="relative bg-primary-dark h-full w-full rounded-full overflow-hidden">
                             <Image src='https://res.cloudinary.com/jethrosama/image/upload/v1651059504/newposthub/profile_pictures/images_ufarco.png' layout="fill" />
                         </div>
@@ -49,6 +51,9 @@ const UserPage = ({ currentUser, uid }) => {
                     { loading ? <Skeleton width={ 60 } /> : user.username }
                 </h1>
             </div>
+            { !loading && user.posts.length === 0 && (<div className='text-center text-offwhite-50 text-lg'>
+                { isOwner ? "You haven't posted anything yet" : `${user.username} hasn't posted anything yet` }
+            </div>) }
             <PostList posts={ user?.posts } loading={ loading } deletePost={ deletePost } currentUser={ currentUser } />
         </div>
     )
