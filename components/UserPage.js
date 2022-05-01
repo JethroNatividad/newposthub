5ea3abcf46ad5029f38f4cb696374674f0a81abb
JethroSama <kel73896@gmail.com>
2022-05-01 20:13:29 +0800

    dynamic show profile image

--- components/UserPage.js
+++ components/UserPage.js
@@ -44,7 +44,7 @@ const UserPage = ({ currentUser, uid }) => {
                 <div className='h-24 w-24 sm:h-32 sm:w-32 lg:h-40 lg:w-40 rounded-full overflow-hidden flex'>
                     { loading ? <Skeleton className='leading-loose' height='160px' width='160px' /> : <div onClick={ () => setProfileModalOpen(true) } className="cursor-pointer text-offwhite-50 w-full h-full flex items-center justify-center bg-tertiary-dark">
                         <div className="relative bg-primary-dark h-full w-full rounded-full overflow-hidden">
-                            <Image src='https://res.cloudinary.com/jethrosama/image/upload/v1651059504/newposthub/profile_pictures/images_ufarco.png' layout="fill" />
+                            <Image src={ currentUser.profilePicture.url } layout="fill" />
                         </div>
                     </div> }
                 </div>
