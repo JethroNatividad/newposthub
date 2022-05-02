ed67a57435b7a289e2f1bad5cab45bcc3d83e487
JethroSama <kel73896@gmail.com>
2022-05-02 17:53:47 +0800

    implement image cropping

--- components/UpdateProfileModal.js
+++ components/UpdateProfileModal.js
@@ -1,10 +1,25 @@
 import { Formik } from 'formik'
 import nprogress from 'nprogress'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { putter } from '../lib/fetcher'
+import ReactCrop from 'react-image-crop'
+import 'react-image-crop/dist/ReactCrop.css'
 
 const UpdateProfileModal = ({ isOpen, setIsOpen, uid }) => {
     const ref = useRef(null)
+    const imgRef = useRef(null)
+    const [imgSrc, setImgSrc] = useState('')
+    const aspect = 1
+    const scale = 1
+    const [completedCrop, setCompletedCrop] = useState()
+    // make crop square
+    const [crop, setCrop] = useState()
+
+    useEffect(() => {
+        console.log(completedCrop, 'complete crop')
+    }, [completedCrop])
+
+
     useEffect(() => {
         /**
          * Alert if clicked on outside of element
@@ -43,17 +58,56 @@ const UpdateProfileModal = ({ isOpen, setIsOpen, uid }) => {
         setSubmitting(false)
     }
 
+    const onImageLoad = (e) => {
+        // set the crop to the center of the image and square
+        const currentHeight = imgRef.current.height
+        const currentWidth = imgRef.current.width
+        const hw = currentHeight < currentWidth ? currentHeight : currentWidth
+        const x = currentHeight === currentWidth ? 0 : currentHeight < currentWidth ? (currentWidth - currentHeight) / 2 : 0
+        const y = currentHeight === currentWidth ? 0 : currentHeight < currentWidth ? 0 : (currentHeight - currentWidth) / 2
+        const imageCrop = {
+            x,
+            y,
+            width: hw,
+            height: hw,
+            unit: 'px'
+        }
+        console.log(imageCrop, 'image crop')
+        setCrop(imageCrop)
+    }
+
     return (
         <div ref={ ref } className={ `${isOpen ? 'flex' : 'hidden'} w-full max-w-3xl h-96 fixed top-13 mx-auto left-0 right-0 z-30 bg-tertiary-dark rounded-md flex justify-center items-center` }>
             <Formik initialValues={ { image: null } }
                 onSubmit={ handleSubmit }>
                 { ({ isSubmitting, handleSubmit, setFieldValue }) => (
                     <form className='flex flex-col items-center space-y-5' onSubmit={ handleSubmit }>
-
                         <input accept='image/*' type="file" multiple={ false } name="image" onChange={ (e) => {
-                            setFieldValue('image', e.target.files[0])
-                            console.log(e.target.files[0], "Image")
+                            if (e.target.files && e.target.files.length > 0) {
+                                setFieldValue('image', e.target.files[0])
+                                console.log(e.target.files[0], "Image")
+                                const reader = new FileReader()
+                                reader.addEventListener('load', () => {
+                                    setImgSrc(reader.result.toString() || '')
+                                }
+                                )
+                                reader.readAsDataURL(e.target.files[0])
+                            }
                         } } />
+                        <ReactCrop
+                            crop={ crop }
+                            onChange={ (_, percentCrop) => setCrop(percentCrop) }
+                            onComplete={ (c) => setCompletedCrop(c) }
+                            aspect={ aspect }
+                        >
+                            <img
+                                ref={ imgRef }
+                                alt="Crop me"
+                                src={ imgSrc }
+                                style={ { transform: `scale(${scale})` } }
+                                onLoad={ onImageLoad }
+                            />
+                        </ReactCrop>
                         <button disabled={ isSubmitting } className='px-4 w-full hover:brightness-110 mb-5 py-3 rounded-lg outline-none text-md md:text-xl font-bold text-offwhite-50 bg-primary-dark' type="submit">Submit</button>
 
                     </form>
